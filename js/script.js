e66eb446daab97a018b7560b693d8bf740793977
nbon <nadabonaso@gmail.com>
2024-03-30 11:43:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-// console.log("Hello world!");
-
-// const myName = "Nada Bonaso";
-// const h1 = document.querySelector(".heading-primary");
-// console.log(myName);
-// console.log(h1);
-
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.fontFamily = "serif";
-//   h1.style.padding = "32px";
-// });
+"use strict";
 
 // Set current year
 const yearEl = document.querySelector(".year");
